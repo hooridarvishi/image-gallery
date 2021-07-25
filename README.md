3035b3fa2c5f11c36176ed17f659da5080e2e3e7
Connor Ocampo <connorocampo@gmail.com>
2021-07-25 09:52:20 -0400

    add pop up functionality

--- README.md
+++ README.md
@@ -9,3 +9,9 @@ The photos used in the gallery were taken from me.
 If you are reading this and know a better, cleaner way to code the functions, please create a [pull request](https://github.com/connorocampo/image-gallery/pulls) and kindly walk me through the reasoning for your PR (pull request).
 
 Thank you!
+
+### Code Credits/References
+
+[James Q Quick's Image Gallery video](https://www.youtube.com/watch?v=vQc3EmFCWRw&t=950s)
+
+- Referenced his code for some styling and JavaScript
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Image filtering functions
+
 function showAll() {
   var galleryImage = document.getElementsByClassName("galleryImage");
   for (let i = 0; i < galleryImage.length; i++) {
@@ -42,3 +44,53 @@ function showChickens() {
 		}
   }
 }
+
+// Nature Pop up 
+let gallery = document.getElementById('gallery');
+let popUp = document.getElementById('popUp');
+let selectedImage = document.getElementById('selectedImage');
+let imageIndexes = [1, 2, 3, 4, 5];
+let selectedIndex = null;
+
+imageIndexes.forEach((i) => {
+  const image = document.createElement('img');
+  image.src = `images/nature-${i}.png`;
+  image.classList.add('galleryImage');
+  image.classList.add('nature');
+
+  image.addEventListener('click', () => {
+    popUp.style.transform = `translateY(0)`;
+    selectedImage.src = `images/nature-${i}.png`;
+    selectedImage.alt = `Florida nature photo ${i}`;
+  })
+
+  gallery.appendChild(image);
+});
+
+
+
+// Chicken Pop up
+let imageChickenIndexes = [1, 2];
+let selectedChickenIndex = null;
+
+imageChickenIndexes.forEach((i) => {
+  const image = document.createElement('img');
+  image.src = `images/chickens-${i}.jpeg`;
+  image.classList.add('galleryImage');
+  image.classList.add('chickens');
+
+  image.addEventListener('click', () => {
+    popUp.style.transform = `translateY(0)`;
+    selectedImage.src = `images/chickens-${i}.jpeg`;
+    selectedImage.alt = `Chickens!`;
+  })
+
+  gallery.appendChild(image);
+});
+
+// Click out of the pop up
+popUp.addEventListener('click' , () => {
+  popUp.style.transform = `translateY(-100%)`;
+  popUp.src = '';
+  popUp.alt = '';
+});
