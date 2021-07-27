51e668debe68768755430e6a938868aa55b7cab0
Connor Ocampo <connorocampo@gmail.com>
2021-07-26 21:54:43 -0400

    add cookie

--- index.js
+++ index.js
@@ -142,14 +142,15 @@ for (let i = 0; i < btns.length; i++) {
   });
 }
 
-// ====================
-// Service Worker
-// ====================
+// ==================================
+// SameSite Cookie and Service Worker
+// ==================================
 
-Cookies.set('name', 'value', {
-  sameSite: 'none',
-  secure: true
-})
+// Set a same-site cookie for first-party contexts
+document.cookie = 'cookie1=value1; SameSite=Lax';
+// Set a cross-site cookie for third-party contexts
+document.cookie = 'cookie2=value2; SameSite=None; Secure';
+
 
 if ("serviceWorker" in navigator) {
   // register service worker
